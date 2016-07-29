f2a189cb4aa66f2f094008cc15482d0d6b2c29c4
saxan_kair <alexandertkach1987@gmail.com>
2016-07-29 22:48:02 +0300

    Workspace changed

---
+++ src/main/java/pages/MyArrayList.java
@@ -0,0 +1,7 @@
+package pages;
+
+/**
+ * Created by admin on 7/25/16.
+ */
+public class MyArrayList {
+}
---
+++ src/test/java/pages/MyArrayListTest.java
@@ -0,0 +1,46 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by admin on 7/25/16.
+ */
+public class MyArrayListTest {
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert1() throws Exception {
+
+    }
+
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
+
+}
